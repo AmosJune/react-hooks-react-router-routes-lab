ad1f0721cf9e1173a7e92d1393351e615e608e4d
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-07-22 16:22:51 +0300

    Done

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { movies } from "../data";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+function Movies() { 
+  const eachMovie = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+      <p>Time: {movie.time} min. </p>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {eachMovie}
+    </div>
+    );
 }
 
 export default Movies;
+
